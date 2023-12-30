6e73d8b9c625b874397154357d4404e30625a55e
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-29 19:37:20 -0500

    Variable for num gpus

--- run_on_all_gpus.sh
+++ run_on_all_gpus.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+num_gpus=$(nvidia-smi --list-gpus | wc -l)
+
 cd /root/lc0/build/release
-for gpu_id in $(seq 0 $(( $(nvidia-smi --list-gpus | wc -l) - 1 ))); do
-  ./lc0-training-client-linux \
-    -backend-opts="cuda-fp16(gpu=$gpu_id)" \
-    -report-host \
-    &
+for gpu_id in $( seq 0 $(( $num_gpus - 1 )) ); do
+  ./lc0-training-client-linux -backend-opts="cuda-fp16(gpu=$gpu_id)" -report-host &
 done
 
 wait
